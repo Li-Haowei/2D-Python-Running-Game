55039a3282975d9dca664709c27d8b5e24fee05b
Haowei Li <91715036+Li-Haowei@users.noreply.github.com>
2023-01-31 19:27:09 -0500

    restart

--- player.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-class Player:
-    def __init__(self, game):
-        self.movement = pygame.Vector2(0, 0)
-        self.game = game
-        self.image = self.game.load_img("assets/images/runner-logo.png")
